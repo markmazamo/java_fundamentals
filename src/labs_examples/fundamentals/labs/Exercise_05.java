2aeeae93013216368020b9e883d7a360ecb8faab
Mark Morgan <m.morgan@mazamocorp.com>
2022-04-15 16:15:34 -0700

    updated exercise 5

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -17,8 +17,12 @@ public class Exercise_05 {
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean b1 = str.equalsIgnoreCase(str2);
+        System.out.println(b1);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String newstring = str + " \n" + str2;
+        System.out.println(newstring);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
