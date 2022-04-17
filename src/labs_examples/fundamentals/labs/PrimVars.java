c1bef92c3af3a9dd92083f3dda286bab89e4f38c
Mark Morgan <m.morgan@mazamocorp.com>
2022-04-17 08:44:07 -0700

    Practice for primitive variables

---
+++ src/labs_examples/fundamentals/labs/PrimVars.java
@@ -0,0 +1,35 @@
+package labs_examples.fundamentals.labs;
+
+public class PrimVars {
+
+    public static void main(String[] args) {
+
+        // please declare one or more variables of each primitive data type and print out their values below
+
+        // example:
+        int i = 10;
+
+        // in the line below we are "concatenating" the value of the int variable "i"
+        // to the the message that will print to the console. So it will print "i = 10"
+        System.out.println("i = " + i);
+
+        // now your turn :)
+        boolean success = true;
+        char c = 'c';
+        byte b = -99;
+        short s = 20000;
+        int j = 1234567890;
+        long l = 1234567890123456789l;
+        float f = 123456.1234567f;
+        double d = 123456.123456789012345;
+
+        System.out.println("success = " + success);
+        System.out.println("c = " + c);
+        System.out.println("b = " + b);
+        System.out.println("s = " + s);
+        System.out.println("j = " + j);
+        System.out.println("l = " + l);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+    }
+}
