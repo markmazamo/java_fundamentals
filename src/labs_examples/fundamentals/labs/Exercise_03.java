c86c3475e0850ed6fabdf3dd3fc4cfc0514298f0
Mark Morgan <m.morgan@mazamocorp.com>
2022-04-16 20:41:53 -0700

    updating Exercise 3

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -13,11 +13,9 @@ package labs_examples.fundamentals.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-
+        //write code here
         System.out.println("Hello World!");
         System.out.println("Check out my first program!");
-        // write code to print out "Check out my first program!" below
-
     }
 
 }
