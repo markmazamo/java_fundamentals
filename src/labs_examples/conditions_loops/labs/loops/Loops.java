bb243c1ac296a8df888357308278a7a17c3833d8
Mark Morgan <m.morgan@mazamocorp.com>
2022-05-11 14:04:40 -0700

    New practice for loops

--- src/labs_examples/conditions_loops/labs/loops/Loops.java
+++ src/labs_examples/conditions_loops/labs/loops/Loops.java
@@ -1,2 +1,11 @@
-package labs_examples.conditions_loops.labs.loops;public class Loops {
+package labs_examples.conditions_loops.labs.loops;
+
+public class Loops {
+    public static void main(String[] args) {
+        // declare a counter variable to use in our while loop (don't forget to increment it as you loop!)
+
+        // please use a while loop to print out every 3rd number between 0 and 100 below
+
+        // please use a do-while loop to print out every 5th number below
+    }
 }
