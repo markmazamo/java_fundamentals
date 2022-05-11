db841069e0a83a5533e2049925a51e14ad53c61d
Mark Morgan <m.morgan@mazamocorp.com>
2022-05-11 14:03:27 -0700

    Practice with if statements

---
+++ src/labs_examples/conditions_loops/labs/if_statements/if_statements.java
@@ -0,0 +1,23 @@
+package labs_examples.conditions_loops.labs.if_statements;
+
+public class if_statements {
+    public static void main(String[] args) {
+        // please write and run at least 3 unique if statements below
+        int i = 10;
+        if (i < 20){
+            System.out.println("i is less than 20!");
+        }
+        // please write and run at least 2 unique if/else-if/else ladders below
+        if (i > 10) {
+            // i is greater than 10
+            System.out.println("i is greater than 10!");
+        } else if (i < 10) {
+            // i is less than 10
+            System.out.println("i is less than 10!");
+        } else {
+            // i is equal to 10
+            System.out.println("i is equal to 10!");
+
+        }
+    }
+}
