c2c2a69f8296dfcfd0362463f677ef3fc2c2776c
Mark Morgan <m.morgan@mazamocorp.com>
2022-05-22 16:27:50 -0700

    New practice for Nested Loops

--- src/labs_examples/conditions_loops/labs/loops/NestedLoops.java
+++ src/labs_examples/conditions_loops/labs/loops/NestedLoops.java
@@ -1,2 +1,21 @@
-package labs_examples.conditions_loops.labs.loops;public class NestedLoops {
+package labs_examples.conditions_loops.labs.loops;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+
+        // please run this application and take a look
+        // at the output to get a good understanding of how
+        // a nested loop operates
+
+        System.out.println("Starting...");
+        for (int i = 0; i < 10; i++){
+            System.out.println("** Outter Loop - i = " + i + " **");
+            for (int j = 0; j < 7; j++){
+                System.out.print("Inner Loop: j = " + j + " | ");
+            }
+            // this is just two line breaks
+            System.out.println("\n\n");
+        }
+        System.out.println("Done!");
+    }
 }
