351e457ed5b0201b2def09b6d5d249db648f999a
Mark Morgan <m.morgan@mazamocorp.com>
2022-05-22 14:40:32 -0700

    Updated Exercise 2 for Loops

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -19,24 +19,36 @@ public class Exercise_02 {
 
         // 1) create scanner (don't forget to import Scanner!)
         Scanner scanner = new Scanner(System.in);
+
         // 2) prompt user
-        System.out.println("Enter a number for the day of the week");
-        int weekday = scanner.nextInt();
+        System.out.println("Enter a number for the day of the week  ");
+
         // 3) assign input to variable as int
+        int weekday = scanner.nextInt();
+
+        // 4) write completed code here
         if (weekday == 1) {
             System.out.println("The day of the week is Sunday");
         }
         else if (weekday == 2) {
-            System.out.println("the day of the week is Monday");
+            System.out.println("The day of the week is Monday");
         }
         else if (weekday == 3) {
-            System.out.println("the day of the week is Tuesday");
+            System.out.println("The day of the week is Tuesday");
         }
         else if (weekday == 4) {
-            System.out.println("the day of the week is Wednesday");
+            System.out.println("The day of the week is Wednesday");
         }
-        System.out.println(number);
-        // 4) write completed code here
+        else if (weekday == 5) {
+            System.out.println("The day of the week is Thursday");
+        }
+        else if (weekday == 6) {
+            System.out.println("The day of the week is Friday");
+        }
+        else if (weekday == 7) {
+            System.out.println("The day of the week is Saturday");
+        }
+
 
     }
 }
