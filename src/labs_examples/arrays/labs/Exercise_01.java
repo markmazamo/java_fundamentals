17964e1f2d041c6bccf8622c08330ee857b3b246
Mark Morgan <m.morgan@mazamocorp.com>
2022-06-14 14:35:13 -0700

    Updated array exercises

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -17,17 +17,21 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         int[] scores = new int[10]; // initialize array for test scores
-
-        // initialize sum variable
-        int sum = 0;
+        int sum = 0; // initialize sum variable
+        int avg = 0; // initial average variable
 
         Scanner scanner = new Scanner(System.in);  // initialize scanner for user input
-        for(int count = 0; count < 10; count++) { // need 10 inputs from the user
-            System.out.print("Enter a number between 1 & 100  ");  // ask for user input
+        for(int count = 0; count < scores.length; count++) {  // need 10 inputs from the user
+            System.out.print("Enter a number between 1 & 100 ");  // ask for user input
             int number = scanner.nextInt();
 
             scores[count] = number;
+            sum = sum + number;  // adding user inputted number to sum
             System.out.println("This is the score " + Arrays.toString(scores) + " for index " + count);
         }
+        System.out.println("This is the sum " + sum);
+
+        avg = sum / scores.length;  // calculate the average
+        System.out.println("This is the average " + avg);
     }
 }
