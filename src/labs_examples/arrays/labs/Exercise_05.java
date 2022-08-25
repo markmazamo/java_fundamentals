4fe0671c6c0e7491e449d5487ab34038feb5c523
Mark Morgan <m.morgan@mazamocorp.com>
2022-08-25 07:24:01 -0700

    updated exer 5

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -9,5 +9,28 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
 
+        // initiate multi-dim array called "table" and populate values using declaration syntax
+        int[][] table = {
+                {10, 20, 30, 40},
+                {50, 60, 70, 80},
+                {90, 100, 110, 120}
+        };
+
+        /**
+        // print out every other element in reverse order using nested for loop
+        for (int i = table.length; i <= 0; i -=2){
+            for (int j = table[i].length; j <= 0; j -=2){
+                System.out.print(table[i][j] + " ");
+            }
+        }
+        */
+
+        // print out every other element in reverse order
+        int x = table.length;               // x = 3
+        int y = table[x] = table.length;    // y = 4
+        System.out.print(table[x][y] + " ");
+
+    }
 }
