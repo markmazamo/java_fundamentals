a26a28207fe38efddabdb24fd9604042af34753e
Mark Morgan <m.morgan@mazamocorp.com>
2022-08-25 06:11:55 -0700

    new exercise 4 completed

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -9,5 +9,18 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+        int[][] scores = {
+                {234, 287},
+                {198, 203, 182},
+                {270}
+        };
+        for (int i = 0; i < scores.length; i++) {               // initial loop index of i
+            for (int j = 0; j < scores[i].length; j++) {        // secondary loop of index j
+                System.out.print(scores[i][j] + " ");           // print the value of the array at index [i][j]
+            }
+            System.out.println();                               //  start a new print line for the next array
 
+        }
+    }
 }
